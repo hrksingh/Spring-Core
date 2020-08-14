e8c612e30cc0fc24cfdc37f5b36bc9e50b2b54b9
Ash <hrksingh17@gmail.com>
2020-08-14 22:19:53 +0530

    Stereotype Annotations @Component and @Value

---
+++ src/main/java/com/atrium/stereotype/Student.java
@@ -0,0 +1,23 @@
+package com.atrium.stereotype;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Component
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class Student {
+	@Value("Ash")
+	private String Name;
+	@Value("Delhi")
+	private String City;
+}
